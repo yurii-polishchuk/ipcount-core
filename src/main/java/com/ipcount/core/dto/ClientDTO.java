ab15667053b44e0b79e030a75bb710dc54f8b10f
Oksana1788 <oksanakl1@i.ua>
2017-04-06 20:11:30 +0300

    Add Clients DTO to expose to frontend only certain data

---
+++ src/main/java/com/ipcount/core/dto/ClientDTO.java
@@ -0,0 +1,157 @@
+package com.ipcount.core.dto;
+
+public class ClientDTO {
+    private Integer id;
+    private Integer statusP;
+    private Integer tPlane;
+    private Integer speedIn;
+    private Integer speedOut;
+    private String ipAddress;
+    private String name;
+    private String phone;
+    private String streetName;
+    private String houseNumber;
+    private String flatNumber;
+    private String mail;
+    private String password;
+    private String sms;
+    private Integer groupC;
+    private Integer speedLoc;
+    private Integer tools;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getStatusP() {
+        return statusP;
+    }
+
+    public void setStatusP(Integer statusP) {
+        this.statusP = statusP;
+    }
+
+    public Integer gettPlane() {
+        return tPlane;
+    }
+
+    public void settPlane(Integer tPlane) {
+        this.tPlane = tPlane;
+    }
+
+    public Integer getSpeedIn() {
+        return speedIn;
+    }
+
+    public void setSpeedIn(Integer speedIn) {
+        this.speedIn = speedIn;
+    }
+
+    public Integer getSpeedOut() {
+        return speedOut;
+    }
+
+    public void setSpeedOut(Integer speedOut) {
+        this.speedOut = speedOut;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSms() {
+        return sms;
+    }
+
+    public void setSms(String sms) {
+        this.sms = sms;
+    }
+
+    public Integer getGroupC() {
+        return groupC;
+    }
+
+    public void setGroupC(Integer groupC) {
+        this.groupC = groupC;
+    }
+
+    public Integer getSpeedLoc() {
+        return speedLoc;
+    }
+
+    public void setSpeedLoc(Integer speedLoc) {
+        this.speedLoc = speedLoc;
+    }
+
+    public Integer getTools() {
+        return tools;
+    }
+
+    public void setTools(Integer tools) {
+        this.tools = tools;
+    }
+}
