2b000ecceff0c5668d663111310d7fb14cd2e4a1
Oksana1788 <oksanakl1@i.ua>
2017-05-15 16:43:21 +0300

    changed 1

--- src/test/java/com/ipcount/core/ApplicationTest.java
+++ src/test/java/com/ipcount/core/ApplicationTest.java
@@ -1,46 +1,95 @@
 package com.ipcount.core;
 
-import com.ipcount.core.controller.ClientController;
+import com.ipcount.core.entity.ClientEntity;
+import com.ipcount.core.repository.ClientRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Date;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ComponentScan("com.ipcount.core")
 public class ApplicationTest {
-
     @Autowired
-    private ClientController controller;
+    private ClientRepository clientRepository;
 
     @Autowired
     private MockMvc mockMvc;
 
 
-    @Test
-    public void notNullTest() throws Exception {
-        assertThat(controller).isNotNull();
+    @Before
+    public void setUp() {
+        ClientEntity clientEntity = new ClientEntity();
+        clientEntity.setStatusP(1);
+        clientEntity.setId(1);
+        clientEntity.setAddress("1");
+        clientEntity.setBalance(1.0);
+        clientEntity.setDate(new Date(12));
+        clientEntity.setFlatNumber("1");
+        clientEntity.setGroupC(1);
+        clientEntity.setHosts("1");
+        clientEntity.setHouseNumber("1");
+        clientEntity.setI("1");
+        clientEntity.setMac("1");
+        clientEntity.setMacR(1);
+        clientEntity.setMail("1");
+        clientEntity.setnDog(1);
+        clientEntity.setName("1");
+        clientEntity.setRule(1);
+        clientEntity.setRuleR(1);
+        clientEntity.setSpeedInR(1);
+        clientEntity.setSpeedLoc(1);
+        clientEntity.setSpeedLocR(1);
+        clientEntity.setSpeedOutR(1);
+        clientEntity.setStatus(1);
+        clientEntity.setStatusP(1);
+        clientEntity.settPlane(1);
+        clientEntity.setSpeedIn(1);
+        clientEntity.setSpeedOut(1);
+        clientEntity.setIpAddress("1");
+        clientEntity.setName("1");
+        clientEntity.setPhone("1");
+        clientEntity.setStreetName("1");
+        clientEntity.setHouseNumber("1");
+        clientEntity.setFlatNumber("1");
+        clientEntity.setMail("1");
+        clientEntity.setPassword("1");
+        clientEntity.setSms("1");
+        clientEntity.setGroupC(1);
+        clientEntity.setSpeedLoc(1);
+        clientEntity.setTools("1");
+        clientRepository.save(clientEntity);
     }
+
 
     @Test
     public void checkResponseBody() throws Exception {
         this.mockMvc.perform(get("/clients")).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("[{\"id\":12,\"statusP\":5,\"tPlane\":8,\"speedIn\":5,\"speedOut\":5,\"ipAddress\":\"45\",\"name\":\"8\",\"phone\":\"9\",\"streetName\":\"5\",\"houseNumber\":\"5\",\"flatNumber\":\"5\",\"mail\":\"5\",\"password\":\"5\",\"sms\":\"q\",\"groupC\":5,\"speedLoc\":5,\"tools\":\"5\"}]")));
+                // .andExpect(status().isOk())
+                .andExpect(content().string(containsString("[{\"id\":1,\"statusP\":1,\"tPlane\":1,\"speedIn\":1,\"speedOut\":1,\"ipAddress\":\"1\",\"name\":\"1\",\"phone\":\"1\",\"streetName\":\"1\",\"houseNumber\":\"1\",\"flatNumber\":\"1\",\"mail\":\"1\",\"password\":\"1\",\"sms\":\"1\",\"groupC\":1,\"speedLoc\":1,\"tools\":\"1\"}]")));
     }
 
+    @After
+    public void tearDown() {
+
+    }
 
 }
